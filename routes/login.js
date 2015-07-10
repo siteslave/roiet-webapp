2c394e404eb0a262621d0a2f93f9b89fc1baf80c
Satit Rianpit <rianpit@gmail.com>
2015-07-10 16:36:53 +0700

    add logout

--- routes/login.js
+++ routes/login.js
@@ -33,4 +33,10 @@ router.post('/dologin', function (req, res) {
 
 });
 
+router.get('/logout', function (req, res) {
+  req.session.destroy(function (err) {
+    res.redirect('/login');
+  });
+});
+
 module.exports = router;
